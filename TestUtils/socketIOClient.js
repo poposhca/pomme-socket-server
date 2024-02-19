5d087984953d19a56194d3e6c5397cf9528228df
Joaquin Popoca <joaquin.popoca@wizeline.com>
2024-02-19 14:24:46 -0600

    feat: Add socket io clients and docker for local env
    New files for running the project in a local environment where added.

---
+++ TestUtils/socketIOClient.js
@@ -0,0 +1,26 @@
+const io = require('socket.io-client');
+
+const port = process.env.PORT || 3000;
+const socket = io(`http://localhost:${port}`, {
+  extraHeaders: {
+    'Authorization': '44',
+  },
+});
+
+socket.on('connect', () => {
+  console.log('Connected to the server');
+});
+
+socket.on('sendQuizPosition', (msg) => {
+    console.log(`Received quiz position`);
+    console.log(msg);
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from the server');
+});
+
+socket.emit('joinQuiz', {
+    quizId: '111',
+    adminId: '466',
+});
